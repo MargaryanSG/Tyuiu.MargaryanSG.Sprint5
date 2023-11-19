dc6c4c77ddd7e20a2bcc9e02d8cc312c00c45add
Tyuiu.MargaryanSG@outlook.com <Tyuiu.MargaryanSG@outlook.com>
2023-11-19 17:34:36 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.MargaryanSG.Sprint5.Task0.V5.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MargaryanSG.Sprint5.Task0.V5.Lib
+{
+    public class DataService : ISprint5Task0V5
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            double z = Math.Log((x + 1)/(x + 2));
+            z = Math.Round(z, 3);
+            File.WriteAllText(path, Convert.ToString(z));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.MargaryanSG.Sprint5.Task0.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MargaryanSG.Sprint5.Task0.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MargaryanSG.Sprint5.Task0.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5e4fe96c-3003-4bdf-83f0-4e2a170d4d15")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
